25d4ce94944ee4fadffefc8ee426ec05f54f3355
Juan de dios <juanfdzg.07@gmail.com>
2026-02-10 18:35:00 -0600

    comit de inicio practica0

---
+++ mipaquete/Practica0.java
@@ -0,0 +1,20 @@
+package mipaquete;
+
+import java.util.Scanner;
+
+public class Practica0 {
+
+    static Scanner input = new Scanner(System.in);
+
+    static void run() {
+        String nombre;
+        System.out.print("Proporciona tu nombre: ");
+        nombre = input.nextLine();
+        System.out.printf("Bienvenido %s al curso de POO\n", nombre);
+    }
+
+    public static void main(String args[]) {
+        run();
+    }
+
+}
